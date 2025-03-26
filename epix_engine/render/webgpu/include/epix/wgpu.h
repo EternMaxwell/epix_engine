979ea3525a27c422cbe9420451d62475d739226a
Etern Maxwell <maxwell_dev@outlook.com>
2025-03-26 17:41:37 +0800

    remove glfw_webgpu submodule. add the create surface function to wgpu.h and in epix::webgpu::utils namespace.

--- epix_engine/render/webgpu/include/epix/wgpu.h
+++ epix_engine/render/webgpu/include/epix/wgpu.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include <webgpu/webgpu.hpp>
+#include <epix/common.h>
+
+struct GLFWwindow;
+
+namespace epix::webgpu::utils {
+    EPIX_API WGPUSurface create_surface(WGPUInstance instance, GLFWwindow* window);
+}
