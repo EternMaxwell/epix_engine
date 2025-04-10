3cb3ed622707cc52b7890b3a669def3f915eca31
Etern Maxwell <maxwell_dev@outlook.com>
2025-04-10 20:55:45 +0800

    bind group entries helper struct.

--- epix_engine/render/render_resources/include/epix/render/resources.h
+++ epix_engine/render/render_resources/include/epix/render/resources.h
@@ -4,9 +4,203 @@
 #include <epix/wgpu.h>
 
 namespace epix::render::resources {
-using RenderAdapter     = wgpu::Adapter;
-using RenderAdapterInfo = wgpu::AdapterInfo;
-using RenderInstance    = wgpu::Instance;
-using RenderDevice      = wgpu::Device;
-using RenderQueue       = wgpu::Queue;
+template <typename T>
+class ArrayProxy {
+   public:
+    constexpr ArrayProxy() noexcept : m_count(0), m_ptr(nullptr) {}
+
+    constexpr ArrayProxy(std::nullptr_t) noexcept
+        : m_count(0), m_ptr(nullptr) {}
+
+    ArrayProxy(T const& value) noexcept : m_count(1), m_ptr(&value) {}
+
+    ArrayProxy(uint32_t count, T const* ptr) noexcept
+        : m_count(count), m_ptr(ptr) {}
+
+    template <std::size_t C>
+    ArrayProxy(T const (&ptr)[C]) noexcept : m_count(C), m_ptr(ptr) {}
+
+#if __GNUC__ >= 9
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winit-list-lifetime"
+#endif
+
+    ArrayProxy(std::initializer_list<T> const& list) noexcept
+        : m_count(static_cast<uint32_t>(list.size())), m_ptr(list.begin()) {}
+
+    template <
+        typename B                                                  = T,
+        typename std::enable_if<std::is_const<B>::value, int>::type = 0>
+    ArrayProxy(
+        std::initializer_list<typename std::remove_const<T>::type> const& list
+    ) noexcept
+        : m_count(static_cast<uint32_t>(list.size())), m_ptr(list.begin()) {}
+
+#if __GNUC__ >= 9
+#pragma GCC diagnostic pop
+#endif
+
+    // Any type with a .data() return type implicitly convertible to T*, and a
+    // .size() return type implicitly convertible to size_t. The const version
+    // can capture temporaries, with lifetime ending at end of statement.
+    template <
+        typename V,
+        typename std::enable_if<
+            std::is_convertible<decltype(std::declval<V>().data()), T*>::
+                value &&
+            std::is_convertible<
+                decltype(std::declval<V>().size()),
+                std::size_t>::value>::type* = nullptr>
+    ArrayProxy(V const& v) noexcept
+        : m_count(static_cast<uint32_t>(v.size())), m_ptr(v.data()) {}
+
+    const T* begin() const noexcept { return m_ptr; }
+
+    const T* end() const noexcept { return m_ptr + m_count; }
+
+    const T& front() const noexcept {
+        VULKAN_HPP_ASSERT(m_count && m_ptr);
+        return *m_ptr;
+    }
+
+    const T& back() const noexcept {
+        assert(m_count && m_ptr);
+        return *(m_ptr + m_count - 1);
+    }
+
+    bool empty() const noexcept { return (m_count == 0); }
+
+    uint32_t size() const noexcept { return m_count; }
+
+    T const* data() const noexcept { return m_ptr; }
+
+   private:
+    uint32_t m_count;
+    T const* m_ptr;
+};
+struct RenderInstance : public wgpu::Instance {
+    RenderInstance() = default;
+    RenderInstance(wgpu::Instance instance) : wgpu::Instance(instance) {}
+    RenderInstance(const RenderInstance&)            = default;
+    RenderInstance(RenderInstance&&)                 = default;
+    RenderInstance& operator=(const RenderInstance&) = default;
+    RenderInstance& operator=(RenderInstance&&)      = default;
+    ~RenderInstance()                                = default;
+};
+struct RenderAdapterInfo : public wgpu::AdapterInfo {
+    RenderAdapterInfo() = default;
+    RenderAdapterInfo(wgpu::AdapterInfo info) : wgpu::AdapterInfo(info) {}
+    RenderAdapterInfo(const RenderAdapterInfo&)            = default;
+    RenderAdapterInfo(RenderAdapterInfo&&)                 = default;
+    RenderAdapterInfo& operator=(const RenderAdapterInfo&) = default;
+    RenderAdapterInfo& operator=(RenderAdapterInfo&&)      = default;
+    ~RenderAdapterInfo()                                   = default;
+};
+struct RenderAdapter : public wgpu::Adapter {
+    RenderAdapter() = default;
+    RenderAdapter(wgpu::Adapter adapter) : wgpu::Adapter(adapter) {}
+    RenderAdapter(const RenderAdapter&)            = default;
+    RenderAdapter(RenderAdapter&&)                 = default;
+    RenderAdapter& operator=(const RenderAdapter&) = default;
+    RenderAdapter& operator=(RenderAdapter&&)      = default;
+    ~RenderAdapter()                               = default;
+};
+using BindGroup = wgpu::BindGroup;
+struct BindGroupDescriptor;
+struct BindingResouce;
+struct BindGroupEntry;
+struct RenderDevice : public wgpu::Device {
+    RenderDevice() = default;
+    RenderDevice(wgpu::Device device) : wgpu::Device(device) {}
+    RenderDevice(const RenderDevice&)            = default;
+    RenderDevice(RenderDevice&&)                 = default;
+    RenderDevice& operator=(const RenderDevice&) = default;
+    RenderDevice& operator=(RenderDevice&&)      = default;
+    ~RenderDevice()                              = default;
+
+    EPIX_API BindGroup createBindGroup(
+        wgpu::BindGroupLayout layout, ArrayProxy<BindGroupEntry> bindings
+    );
+};
+struct RenderQueue : public wgpu::Queue {
+    RenderQueue() = default;
+    RenderQueue(wgpu::Queue queue) : wgpu::Queue(queue) {}
+    RenderQueue(const RenderQueue&)            = default;
+    RenderQueue(RenderQueue&&)                 = default;
+    RenderQueue& operator=(const RenderQueue&) = default;
+    RenderQueue& operator=(RenderQueue&&)      = default;
+    ~RenderQueue()                             = default;
+};
+struct BufferBinding {
+    wgpu::Buffer buffer;
+    uint64_t offset = 0;
+    uint64_t size   = 0;
+};
+using Sampler     = wgpu::Sampler;
+using TextureView = wgpu::TextureView;
+using Buffer      = wgpu::Buffer;
+struct BindingResouce {
+    std::variant<
+        BufferBinding,
+        std::vector<Buffer>,
+        Sampler,
+        std::vector<Sampler>,
+        TextureView,
+        std::vector<TextureView>>
+        resource;
+
+    BindingResouce(Buffer buffer, uint64_t offset = 0, uint64_t size = 0) {
+        resource =
+            BufferBinding{buffer, offset, size ? size : buffer.getSize()};
+    }
+    BindingResouce(ArrayProxy<Buffer> buffers) {
+        resource = std::vector<Buffer>(
+            buffers.data(), buffers.data() + buffers.size()
+        );
+    }
+    BindingResouce(Sampler sampler) { resource = sampler; }
+    BindingResouce(ArrayProxy<Sampler> samplers) {
+        resource = std::vector<Sampler>(
+            samplers.data(), samplers.data() + samplers.size()
+        );
+    }
+    BindingResouce(TextureView view) { resource = view; }
+    BindingResouce(ArrayProxy<TextureView> views) {
+        resource =
+            std::vector<TextureView>(views.data(), views.data() + views.size());
+    }
+};
+struct BindGroupEntry {
+    uint32_t binding;
+    BindingResouce resource;
+};
+// Helper for creating bind group entries
+struct BindGroupEntries {
+    template <typename... Args>
+    static std::array<BindGroupEntry, sizeof...(Args)> sequence(Args&&... args
+    ) {
+        std::array<BindGroupEntry, sizeof...(Args)> entries{
+            BindGroupEntry{0, BindingResouce(std::forward<Args>(args))}...
+        };
+        for (size_t index = 0; index < sizeof...(Args); index++) {
+            entries[index].binding = index;
+        }
+        return std::move(entries);
+    }
+    template <typename T, typename... Args>
+        requires std::constructible_from<BindingResouce, T>
+    static std::vector<BindGroupEntry> with_indices(
+        uint32_t index, T&& resource, Args&&... args
+    ) {
+        std::vector<BindGroupEntry> entries{BindGroupEntry{
+            static_cast<uint32_t>(index),
+            BindingResouce(std::forward<T>(resource))
+        }};
+        if constexpr (sizeof...(args) > 0) {
+            auto appendees = with_indices(std::forward<Args>(args)...);
+            entries.insert(entries.end(), appendees.begin(), appendees.end());
+        }
+        return std::move(entries);
+    }
+};
 }  // namespace epix::render::resources
--- epix_engine/render/render_resources/src/resources.cpp
+++ epix_engine/render/render_resources/src/resources.cpp
@@ -1,1 +1,80 @@
 #include "epix/render/resources.h"
+
+namespace epix::render::resources {
+EPIX_API BindGroup RenderDevice::createBindGroup(
+    wgpu::BindGroupLayout layout, ArrayProxy<BindGroupEntry> bindings
+) {
+    std::vector<wgpu::BindGroupEntry> entries;
+    std::vector<wgpu::BindGroupEntryExtras> extras;
+    entries.reserve(bindings.size());
+    extras.reserve(bindings.size());  // reserve for extras
+    for (auto&& entry : bindings) {
+        entries.emplace_back();
+        entries.back().binding = entry.binding;
+        auto&& res             = entry.resource.resource;
+        if (std::holds_alternative<BufferBinding>(res)) {
+            // buffer binding
+            auto&& bufferBinding  = std::get<BufferBinding>(res);
+            entries.back().buffer = bufferBinding.buffer;
+            entries.back().offset = bufferBinding.offset;
+            entries.back().size   = bufferBinding.size;
+        } else if (std::holds_alternative<std::vector<Buffer>>(res)) {
+            // buffer binding array
+            auto&& bufferBindings = std::get<std::vector<Buffer>>(res);
+            extras.emplace_back();
+            extras.back().bufferCount =
+                static_cast<uint32_t>(bufferBindings.size());
+            extras.back().buffers      = (WGPUBuffer*)bufferBindings.data();
+            entries.back().nextInChain = &extras.back().chain;
+        } else if (std::holds_alternative<Sampler>(res)) {
+            // sampler
+            auto&& sampler         = std::get<Sampler>(res);
+            entries.back().sampler = sampler;
+        } else if (std::holds_alternative<std::vector<Sampler>>(res)) {
+            // sampler array
+            auto&& samplers = std::get<std::vector<Sampler>>(res);
+            extras.emplace_back();
+            extras.back().samplerCount = static_cast<uint32_t>(samplers.size());
+            extras.back().samplers     = (WGPUSampler*)samplers.data();
+        } else if (std::holds_alternative<TextureView>(res)) {
+            // texture view
+            auto&& textureView         = std::get<TextureView>(res);
+            entries.back().textureView = textureView;
+        } else if (std::holds_alternative<std::vector<TextureView>>(res)) {
+            // texture view array
+            auto&& textureViews = std::get<std::vector<TextureView>>(res);
+            extras.emplace_back();
+            extras.back().textureViewCount =
+                static_cast<uint32_t>(textureViews.size());
+            extras.back().textureViews = (WGPUTextureView*)textureViews.data();
+            entries.back().nextInChain = &extras.back().chain;
+        }
+    }
+    wgpu::BindGroupDescriptor bindGroupDescriptor;
+    bindGroupDescriptor.layout     = layout;
+    bindGroupDescriptor.entryCount = static_cast<uint32_t>(entries.size());
+    bindGroupDescriptor.entries    = entries.data();
+    auto bindGroup = wgpu::Device::createBindGroup(bindGroupDescriptor);
+    return bindGroup;
+}
+
+// test for compiling
+void test1() {
+    RenderDevice device;
+    BindGroup bindGroup = device.createBindGroup(
+        wgpu::BindGroupLayout(),
+        BindGroupEntries::sequence(
+            Buffer(), std::vector<Buffer>(), Sampler(), std::vector<Sampler>(),
+            TextureView(), std::vector<TextureView>()
+        )
+    );
+    bindGroup = device.createBindGroup(
+        wgpu::BindGroupLayout(),
+        BindGroupEntries::with_indices(
+            0, Buffer(), 1, std::vector<Buffer>(), 2, Sampler(), 3,
+            std::vector<Sampler>(), 4, TextureView(), 5,
+            std::vector<TextureView>()
+        )
+    );
+}
+}  // namespace epix::render::resources
