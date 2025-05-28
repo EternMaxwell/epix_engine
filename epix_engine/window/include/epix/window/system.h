8a5f0482d69100e15f7ce8c6c9ee3586829cfba4
Etern Maxwell <maxwell_dev@outlook.com>
2025-05-28 16:26:22 +0800

    exit only one the internal windows are destroyed, so that other systems can have at least one frame to react to window close event before exiting the app. this is useful for render world resource destruction since exit schedules doesnot work on render world.

--- epix_engine/window/include/epix/window/system.h
+++ epix_engine/window/include/epix/window/system.h
@@ -6,11 +6,16 @@
 namespace epix::window {
 struct PrimaryWindow {};
 EPIX_API void exit_on_all_closed(
-    EventWriter<AppExit>& exit_writer, Query<Get<window::Window>>& query
+    EventWriter<AppExit>& exit_writer,
+    Local<entt::dense_set<Entity>> still_alive,
+    EventReader<events::WindowCreated> created,
+    EventReader<events::WindowDestroyed> destroyed
 );
 EPIX_API void exit_on_primary_closed(
     EventWriter<AppExit>& exit_writer,
-    Query<Get<window::Window>, With<PrimaryWindow>>& query
+    Query<Get<Entity>, With<window::Window, PrimaryWindow>>& query,
+    Local<std::optional<Entity>> primary_window,
+    EventReader<events::WindowDestroyed> destroyed
 );
 EPIX_API void close_requested(
     Commands& commands,
