973e40c51efad0626e967e743aa292eb2c19f060
Etern Maxwell <maxwell_dev@outlook.com>
2025-09-29 00:12:05 +0800

    table and type registry bug fixes, added type info in type registry that might be useful if trying to use blobvec for table and sparseset storage.

---
+++ epix_engine/core/include/epix/core/storage/table.hpp
@@ -0,0 +1,174 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <ranges>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+
+#include "dense.hpp"
+#include "fwd.hpp"
+#include "sparse_array.hpp"
+#include "sparse_set.hpp"
+
+namespace epix::core::storage {
+struct Table {
+   private:
+    SparseSet<size_t, DenseInterface> _denses;
+    std::vector<Entity> _entities;
+    friend struct Tables;
+
+   public:
+    auto entities(this const Table& self) { return std::views::all(self._entities); }
+    size_t capacity(this const Table& self) { return self._entities.capacity(); }
+    size_t size(this const Table& self) { return self._entities.size(); }
+    size_t type_count(this const Table& self) { return self._denses.size(); }
+    bool empty(this const Table& self) { return self._entities.empty(); }
+    void reserve(this Table& self, size_t additional) {
+        self._entities.reserve(self._entities.size() + additional);
+        for (auto&& [_, dense] : self._denses.iter_mut()) {
+            dense.reserve(additional);
+        }
+    }
+    bool has_dense(this const Table& self, size_t type_id) { return self._denses.contains(type_id); }
+    /**
+     * @brief Removes the components at the given dense index, and swap the last component into its place.
+     *
+     * @param dense_index The dense index of the component to remove.
+     * @return std::optional<Entity> The entity that is swapped into the removed component's place.
+     */
+    std::optional<Entity> swap_remove(this Table& self, size_t dense_index) {
+        assert(dense_index < self._entities.size());
+        bool is_last = dense_index == self._entities.size() - 1;
+        for (auto&& [type_id, dense] : self._denses.iter_mut()) {
+            dense.swap_remove(dense_index);
+        }
+        std::swap(self._entities[dense_index], self._entities.back());
+        self._entities.pop_back();
+        if (!is_last) {
+            return self._entities[dense_index];
+        } else {
+            return std::nullopt;
+        }
+    }
+    struct MoveReturn {
+        size_t new_index;
+        std::optional<Entity> swapped_entity;
+    };
+    MoveReturn move_to(this Table& self, size_t dense_index, Table& target) {
+        size_t new_index = target._entities.size();
+        for (auto&& [type_id, dense] : self._denses.iter_mut()) {
+            target._denses.get_mut(type_id).and_then([&](DenseInterface& target_dense) -> std::optional<bool> {
+                dense.get_data_mut(dense_index).and_then([&](void* value) -> std::optional<bool> {
+                    target_dense.push_move({0, 0}, value);
+                    return true;
+                });
+                return true;
+            });
+        }
+        bool is_last = dense_index == self._entities.size() - 1;
+        std::swap(self._entities[dense_index], self._entities.back());
+        target._entities.push_back(self._entities.back());
+        self._entities.pop_back();
+        if (!is_last) {
+            return {new_index, self._entities[dense_index]};
+        } else {
+            return {new_index, std::nullopt};
+        }
+    }
+    std::optional<std::pair<const void*, const void*>> get_data_for(this const Table& self, size_t type_id) {
+        return self._denses.get(type_id).transform([](const DenseInterface& dense) { return dense.get_data(); });
+    }
+    template <typename T>
+    std::optional<std::span<const T>> get_data_as_for(this const Table& self, size_t type_id) {
+        return self._denses.get(type_id).transform([](const DenseInterface& dense) { return dense.get_data_as<T>(); });
+    }
+    std::optional<std::span<const Tick>> get_added_ticks_for(this const Table& self, size_t type_id) {
+        return self._denses.get(type_id).transform([](const DenseInterface& dense) { return dense.get_added_ticks(); });
+    }
+    std::optional<std::span<const Tick>> get_modified_ticks_for(this const Table& self, size_t type_id) {
+        return self._denses.get(type_id).transform(
+            [](const DenseInterface& dense) { return dense.get_modified_ticks(); });
+    }
+    std::optional<const void*> get_data(this const Table& self, size_t type_id, uint32_t index) {
+        return self._denses.get(type_id).and_then([&](const DenseInterface& dense) { return dense.get_data(index); });
+    }
+    std::optional<std::reference_wrapper<const Tick>> get_added_tick(this const Table& self,
+                                                                     size_t type_id,
+                                                                     uint32_t index) {
+        return self._denses.get(type_id).and_then(
+            [&](const DenseInterface& dense) { return dense.get_added_tick(index); });
+    }
+    std::optional<std::reference_wrapper<const Tick>> get_modified_tick(this const Table& self,
+                                                                        size_t type_id,
+                                                                        uint32_t index) {
+        return self._denses.get(type_id).and_then(
+            [&](const DenseInterface& dense) { return dense.get_modified_tick(index); });
+    }
+    std::optional<ComponentTicks> get_ticks(this const Table& self, size_t type_id, uint32_t index) {
+        return self._denses.get(type_id).and_then([&](const DenseInterface& dense) { return dense.get_ticks(index); });
+    }
+
+    std::optional<std::reference_wrapper<const DenseInterface>> get_dense(this const Table& self, size_t type_id) {
+        return self._denses.get(type_id).transform([](const DenseInterface& dense) { return std::cref(dense); });
+    }
+    std::optional<std::reference_wrapper<DenseInterface>> get_dense_mut(this Table& self, size_t type_id) {
+        return self._denses.get_mut(type_id).transform([](DenseInterface& dense) { return std::ref(dense); });
+    }
+};
+struct Tables {
+    struct VecHash {
+        size_t operator()(const std::vector<size_t>& vec) const {
+            size_t hash = 0;
+            for (size_t v : vec) {
+                hash ^= std::hash<size_t>()(v) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+            }
+            return hash;
+        }
+    };
+
+   private:
+    std::shared_ptr<type_system::TypeRegistry> _type_registry;
+    std::unordered_map<std::vector<size_t>, size_t, VecHash> _table_id_registry;
+    std::vector<Table> _tables;
+
+   public:
+    size_t table_count(this const Tables& self) { return self._tables.size(); }
+    bool empty(this const Tables& self) { return self._tables.empty(); }
+    std::optional<std::reference_wrapper<const Table>> get(this const Tables& self, size_t table_id) {
+        if (table_id >= self._tables.size()) {
+            return std::nullopt;
+        }
+        return std::cref(self._tables[table_id]);
+    }
+    std::optional<std::reference_wrapper<Table>> get_mut(this Tables& self, size_t table_id) {
+        if (table_id >= self._tables.size()) {
+            return std::nullopt;
+        }
+        return std::ref(self._tables[table_id]);
+    }
+    std::optional<size_t> get_table_id(this const Tables& self, const std::vector<size_t>& type_ids) {
+        return self._table_id_registry.find(type_ids) != self._table_id_registry.end()
+                   ? std::optional(self._table_id_registry.at(type_ids))
+                   : std::nullopt;
+    }
+    template <typename... Components>
+    std::optional<size_t> get_table_id(this const Tables& self) {
+        std::vector<size_t> type_ids = {self._type_registry->type_id<Components>()...};
+        return self.get_table_id(type_ids);
+    }
+    template <typename... Components>
+    size_t get_table_id_or_insert(this Tables& self) {
+        std::vector<size_t> type_ids = {self._type_registry->type_id<Components>()...};
+        size_t table_id              = self.get_table_id(type_ids).value_or([&]() {
+            Table table;
+            (table._denses.emplace(self._type_registry->type_id<Components>(), DenseInterface(Dense<Components>())),
+             ...);
+            self._tables.emplace_back(std::move(table));
+            return self._tables.size() - 1;
+        }());
+        return table_id;
+    }
+};
+}  // namespace epix::core::storage
--- epix_engine/core/include/epix/core/type_system/type_registry.hpp
+++ epix_engine/core/include/epix/core/type_system/type_registry.hpp
@@ -1,20 +1,109 @@
 #pragma once
 
 #include <atomic>
+#include <csetjmp>
+#include <cstring>
+#include <memory>
+#include <type_traits>
 #include <unordered_map>
+#include <vector>
 
 #include "../meta/typeindex.hpp"
 #include "fwd.hpp"
 
 namespace epix::core::type_system {
+struct TypeInfo {
+    std::string_view name;
+    size_t size;
+    size_t align;
+    bool movable;
+    void (*move)(void* dest, void* src);  // If movable but this is null, that means this type is trivially movable.
+    bool copyable;
+    void (*copy)(void* dest,
+                 const void* src);  // If copyable but this is null, that means this type is trivially copyable.
+    bool destructible;
+    void (*destroy)(void* ptr);              // If null, that means this type is trivially destructible.
+    void (*destroy_n)(void* ptr, size_t n);  // If null, that means this type is trivially destructible.
+    void (*uninitialized_move_n)(
+        void* src, size_t n, void* dest);  // If movable but this is null, that means this type is trivially movable.
+    void (*uninitialized_copy_n)(
+        const void* src,
+        size_t n,
+        void* dest);  // If copyable but this is null, that means this type is trivially copyable.
+
+    template <typename T>
+    static TypeInfo make_info() {
+        return TypeInfo {
+            .name = epix::core::meta::type_id<T>{}.name(), .size = sizeof(T), .align = alignof(T),
+            .movable      = std::is_move_constructible_v<T>,
+            .move         = std::is_trivially_move_constructible<T>::value
+                                ? nullptr
+                                : (std::is_move_constructible_v<T>
+                                       ? [](void* dest, void* src) { new (dest) T(std::move(*static_cast<T*>(src))); }
+                                       : nullptr),
+            .copyable     = std::is_copy_constructible_v<T>,
+            .copy         = std::is_trivially_copy_constructible<T>::value
+                                ? nullptr
+                                : (std::is_copy_constructible_v<T>
+                                       ? [](void* dest, const void* src) { new (dest) T(*static_cast<const T*>(src)); }
+                                       : nullptr),
+            .destructible = std::is_destructible_v<T>,
+            .destroy      = std::is_trivially_destructible_v<T>
+                                ? nullptr
+                                : (std::is_destructible_v<T> ? [](void* ptr) { static_cast<T*>(ptr)->~T(); } : nullptr),
+            .destroy_n    = std::is_trivially_destructible_v<T>
+                                ? nullptr
+                                : [](void* ptr, size_t n) {
+                                      T* typed_ptr = static_cast<T*>(ptr);
+                                      for (size_t i = 0; i < n; i++) {
+                                          typed_ptr[i].~T();
+                                      }
+                                  },
+            .uninitialized_move_n = std::is_trivially_move_constructible<T>::value
+                                        ? nullptr
+                                        : [](void* src, size_t n, void* dest) {
+                                              T* src_t  = static_cast<T*>(src);
+                                              T* dest_t = static_cast<T*>(dest);
+                                              std::uninitialized_move_n(src_t, n, dest_t);
+                                          },
+            .uninitialized_copy_n = std::is_trivially_copy_constructible<T>::value
+                                        ? nullptr
+                                        : [](const void* src, size_t n, void* dest) {
+                                              const T* src_t = static_cast<const T*>(src);
+                                              T* dest_t     = static_cast<T*>(dest);
+                                              std::uninitialized_copy_n(src_t, n, dest_t);
+                                          },
+        };
+    }
+};
 struct TypeRegistry {
    private:
+    mutable std::vector<TypeInfo> typeInfos;
     mutable size_t nextId = 0;
     mutable std::unordered_map<const char*, size_t> types;
     mutable std::unordered_map<std::string_view, size_t> typeViews;
 
     mutable std::atomic<size_t> readers{0};
     mutable std::atomic<int> state{0};  // 0: idle, 1: waiting write
+
+    void lock_read() const {
+        while (true) {
+            int expected_idle = 0;
+            if (state.compare_exchange_weak(expected_idle, 0)) {
+                readers.fetch_add(1);
+                break;
+            }
+        }
+    }
+    void unlock_read() const { readers.fetch_sub(1); }
+    void lock_write() const {
+        state.store(1);  // lock state, waiting to write.
+        size_t expected_readers = 0;
+        while (readers.compare_exchange_weak(expected_readers, 0)) {
+            expected_readers = 0;
+        }
+    }
+    void unlock_write() const { state.store(0); }
 
    public:
     TypeRegistry()  = default;
@@ -23,37 +112,35 @@ struct TypeRegistry {
     template <typename T = void>
     size_t type_id(const epix::core::meta::type_index& index = epix::core::meta::type_id<T>()) const {
         // change state to reading
-        while (true) {
-            int expected_idle = 0;
-            if (state.compare_exchange_weak(expected_idle, 2)) {
-                readers.fetch_add(1);
-                break;
-            }
-        }
+        lock_read();
         // If in types
         if (auto it = types.find(index.name().data()); it != types.end()) {
             size_t id = it->second;
-            readers.fetch_sub(1);
+            unlock_read();
             return id;
         } else {
             // Not in types, need write
-            readers.fetch_sub(1);
-            state.store(1);  // lock state, waiting to write.
-            size_t expected_readers = 0;
-            while (readers.compare_exchange_weak(expected_readers, 0)) {
-                expected_readers = 0;
-            }
+            unlock_read();
+            lock_write();
+            size_t id;
             if (auto itv = typeViews.find(index.name()); itv != typeViews.end()) {
                 types[index.name().data()] = itv->second;
-                return itv->second;
+                id                         = itv->second;
             } else {
-                size_t id                  = nextId++;
+                id                         = nextId++;
                 typeViews[index.name()]    = id;
                 types[index.name().data()] = id;
-                return id;
+                typeInfos.push_back(TypeInfo::make_info<T>());
             }
-            state.store(0);
+            unlock_write();
+            return id;
         }
+    }
+    TypeInfo type_info(size_t type_id) const {
+        lock_read();
+        TypeInfo info = typeInfos[type_id];
+        unlock_read();
+        return info;
     }
 };
 }  // namespace epix::core::type_system
