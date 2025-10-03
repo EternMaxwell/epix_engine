2948c481f5f0344aac80af30816561b425c9f97e
Etern Maxwell <maxwell_dev@outlook.com>
2025-10-04 01:18:27 +0800

    complete component header, add Components and ComponentInfo, but might add more features here.

--- epix_engine/core/include/epix/core/component.hpp
+++ epix_engine/core/include/epix/core/component.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <functional>
 #include <unordered_map>
+#include <unordered_set>
 
 #include "entities.hpp"
 #include "fwd.hpp"
+#include "storage/sparse_set.hpp"
+#include "storage/table.hpp"
+#include "type_system/type_registry.hpp"
 
 namespace epix::core {
 struct HookContext {
@@ -80,14 +85,65 @@ struct ComponentHooks {
     }
 };
 struct RequiredComponent {
-    
+    // function to construct the component for the target entity, the size_t is the component index inside table.
+    std::function<void(storage::Tables&, storage::SparseSets&, Tick, size_t, Entity)> constructor;
     uint16_t inheritance_depth = 0;
+};
+struct RequiredComponents {
+    std::unordered_map<size_t, RequiredComponent> components;
+};
+enum class StorageType : uint8_t {
+    Table     = 0,  // default stored in tables
+    SparseSet = 1,
+};
+template <typename T>
+StorageType get_storage_type_for() {
+    if constexpr (requires {
+                      { T::storage_type() } -> std::same_as<StorageType>;
+                  }) {
+        return T::storage_type();
+    } else {
+        return StorageType::Table;
+    }
+}
+struct ComponentDesc {
+   private:
+    const type_system::TypeInfo* _type_info;
+    StorageType _storage_type;
+
+    ComponentDesc(const type_system::TypeInfo* type_info, StorageType storage_type)
+        : _type_info(type_info), _storage_type(storage_type) {}
+
+   public:
+    template <typename T>
+    static ComponentDesc from_type() {
+        return ComponentDesc(type_system::TypeInfo::get_info<T>(), get_storage_type_for<T>());
+    }
+    static ComponentDesc from_info(const type_system::TypeInfo* type_info, StorageType storage_type) {
+        return ComponentDesc(type_info, storage_type);
+    }
+
+    const type_system::TypeInfo* type_info() const { return _type_info; }
+    StorageType storage_type() const { return _storage_type; }
 };
 struct ComponentInfo {
    private:
-    size_t id;
-    ComponentHooks hooks;
+    size_t _id;
+    ComponentDesc _desc;
+    ComponentHooks _hooks;
+    RequiredComponents _required_components;
+    std::unordered_set<size_t> _required_by;
 
    public:
+    ComponentInfo(size_t id, const ComponentDesc& desc) : _id(id), _desc(desc) {}
+
+    size_t type_id() const { return _id; }
+    const type_system::TypeInfo* type_info() const { return _desc.type_info(); }
+    StorageType storage_type() const { return _desc.storage_type(); }
+    const ComponentHooks& hooks() const { return _hooks; }
+    const RequiredComponents& required_components() const { return _required_components; }
+};
+struct Components : public storage::SparseSet<size_t, ComponentInfo> {
+    using storage::SparseSet<size_t, ComponentInfo>::SparseSet;
 };
 }  // namespace epix::core
