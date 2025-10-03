975d517c5c84c31fd70ec94c7854f9c16378a127
Etern Maxwell <maxwell_dev@outlook.com>
2025-10-04 01:18:42 +0800

    Storage for World.

--- epix_engine/core/include/epix/core/storage.hpp
+++ epix_engine/core/include/epix/core/storage.hpp
@@ -1,2 +1,22 @@
 #pragma once
 
+#include "fwd.hpp"
+#include "storage/fwd.hpp"
+#include "storage/resource.hpp"
+#include "storage/sparse_set.hpp"
+#include "storage/table.hpp"
+#include "component.hpp"
+
+namespace epix::core {
+struct Storage {
+    storage::SparseSets sparse_sets;
+    storage::Tables tables;
+    storage::Resources resources;
+
+    void prepare_component(const ComponentInfo& info) {
+        if (info.storage_type() == StorageType::SparseSet) {
+            sparse_sets.get_or_insert(info.type_id());
+        }
+    }
+};
+}
