a7c17bccd3ed0c684b7bc238dfce3644b8643e0a
Etern Maxwell <maxwell_dev@outlook.com>
2025-10-18 22:53:56 +0800

    remake EPIX_MAKE_INT_WRAPPER to more general usage.

--- epix_engine/core/include/epix/api/macros.hpp
+++ epix_engine/core/include/epix/api/macros.hpp
@@ -19,39 +19,60 @@
 #define EPIX_API
 #endif
 
-#ifndef EPIX_MAKE_U32_WRAPPER
-#define EPIX_MAKE_U32_WRAPPER(type)                                                       \
-    struct type {                                                                         \
-       private:                                                                           \
-        uint32_t value = 0;                                                               \
-                                                                                          \
-       public:                                                                            \
-        constexpr type(uint32_t v = 0) : value(v) {}                                      \
-        constexpr uint32_t get(this const type& self) { return self.value; }              \
-        constexpr void set(this type& self, uint32_t v) { self.value = v; }               \
-        constexpr auto operator<=>(const type&) const = default;                          \
-        constexpr operator uint32_t() const { return value; }                             \
-        constexpr operator size_t() const { return static_cast<size_t>(value); }          \
-    };                                                                                    \
-    struct hash_##type {                                                                  \
-        size_t operator()(const type& v) const { return std::hash<uint32_t>()(v.get()); } \
+#include <concepts>
+#include <cstdint>
+#include <utility>
+
+namespace epix::core::wrapper {
+template <typename T>
+    requires std::is_integral_v<T>
+struct int_base {
+   public:
+    using value_type = T;
+
+    constexpr int_base(T v = 0) : value(v) {}
+    constexpr T get(this int_base self) { return self.value; }
+    constexpr void set(this int_base& self, T v) { self.value = v; }
+    constexpr auto operator<=>(const int_base&) const = default;
+    constexpr operator T() const { return value; }
+    constexpr operator size_t()
+        requires(!std::same_as<T, size_t>)
+    {
+        return static_cast<size_t>(value);
+    }
+
+   protected:
+    T value;
+};
+}  // namespace epix::core::wrapper
+
+namespace std {
+template <typename T>
+    requires std::is_integral_v<T>
+struct hash<epix::core::wrapper::int_base<T>> {
+    size_t operator()(const epix::core::wrapper::int_base<T>& v) const { return std::hash<T>()(v.get()); }
+};
+template <typename T>
+    requires std::derived_from<T, epix::core::wrapper::int_base<typename T::value_type>>
+struct hash<T> {
+    size_t operator()(const T& v) const {
+        return std::hash<epix::core::wrapper::int_base<typename T::value_type>>()(v);
+    }
+};
+}  // namespace std
+
+#ifndef EPIX_MAKE_INT_WRAPPER
+#define EPIX_MAKE_INT_WRAPPER(type, int_type)                        \
+    struct type : public ::epix::core::wrapper::int_base<int_type> { \
+       public:                                                       \
+        using ::epix::core::wrapper::int_base<int_type>::int_base;   \
     };
 #endif
 
+#ifndef EPIX_MAKE_U32_WRAPPER
+#define EPIX_MAKE_U32_WRAPPER(type) EPIX_MAKE_INT_WRAPPER(type, uint32_t)
+#endif
+
 #ifndef EPIX_MAKE_U64_WRAPPER
-#define EPIX_MAKE_U64_WRAPPER(type)                                                       \
-    struct type {                                                                         \
-       private:                                                                           \
-        uint64_t value = 0;                                                               \
-                                                                                          \
-       public:                                                                            \
-        constexpr type(uint64_t v = 0) : value(v) {}                                      \
-        constexpr uint64_t get(this const type& self) { return self.value; }              \
-        constexpr void set(this type& self, uint64_t v) { self.value = v; }               \
-        constexpr auto operator<=>(const type&) const = default;                          \
-        constexpr operator size_t() const { return static_cast<size_t>(value); }          \
-    };                                                                                    \
-    struct hash_##type {                                                                  \
-        size_t operator()(const type& v) const { return std::hash<uint64_t>()(v.get()); } \
-    };
+#define EPIX_MAKE_U64_WRAPPER(type) EPIX_MAKE_INT_WRAPPER(type, uint64_t)
 #endif
