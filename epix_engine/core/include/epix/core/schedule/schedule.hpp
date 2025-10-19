7dcff4e595295768e85548cf4ee31617a51a94d9
Etern Maxwell <maxwell_dev@outlook.com>
2025-10-19 23:12:11 +0800

    SystemDispatcher used for schedule. Schedule. Added BS::thread_pool for SystemDispatcher to run systems on multithread.

---
+++ epix_engine/core/include/epix/core/schedule/schedule.hpp
@@ -0,0 +1,284 @@
+#pragma once
+
+#include <algorithm>
+#include <cstddef>
+#include <deque>
+#include <expected>
+#include <ranges>
+
+#include "../system/system.hpp"
+#include "system_dispatcher.hpp"
+#include "system_set.hpp"
+
+namespace epix::core::schedule {
+struct Node;
+struct Edges {
+    std::unordered_set<SystemSetLabel> depends;
+    std::unordered_set<SystemSetLabel> successors;
+    std::unordered_set<SystemSetLabel> parents;
+    std::unordered_set<SystemSetLabel> children;
+};
+struct Node {
+    SystemSetLabel label;
+    system::SystemUnique<> system;
+    query::FilteredAccessSet system_access;
+    std::vector<system::SystemUnique<std::tuple<>, bool>> conditions;
+    query::FilteredAccessSet condition_access;
+
+    Edges edges;
+    Edges validated_edges;
+};
+struct CachedNode {
+    std::shared_ptr<Node> node;  // stores shared_ptr so you can safely add, remove nodes while executing
+    std::vector<size_t> depends;
+    std::vector<size_t> successors;
+    std::vector<size_t> parents;
+    std::vector<size_t> children;
+};
+struct ScheduleCache {
+    std::vector<CachedNode> nodes;
+    std::unordered_map<SystemSetLabel, size_t> node_map;
+    std::vector<size_t> start_nodes;
+};
+struct ExecutionState {
+    size_t running_count   = 0;
+    size_t remaining_count = 0;
+    storage::bit_vector ready_nodes;
+    storage::bit_vector running_nodes;
+    storage::bit_vector finished_nodes;
+    storage::bit_vector entered_nodes;
+    storage::bit_vector condition_met_nodes;
+    std::vector<size_t> wait_count;  // number of dependencies + parents not yet satisfied
+    std::vector<size_t> child_count;
+};
+struct SchedulePrepareError {
+    // labels involved in the error, 0 will be the same child if type is ParentsWithDeps
+    std::vector<SystemSetLabel> associated_labels;
+    // for ParentsWithDeps, the parents the have dependencies
+    std::vector<SystemSetLabel> conflict_parents;
+    enum class Type {
+        CyclicDependency,
+        CyclicHierarchy,
+        ParentsWithDeps,
+    } type;
+};
+struct Schedule {
+    std::unordered_map<SystemSetLabel, std::shared_ptr<Node>> nodes;
+    std::optional<ScheduleCache> cache;
+
+    // prepare the schedule (validate and build cache), if check_error is true, will check for errors
+    // otherwise the error will cause skipped nodes during execution
+    std::expected<void, SchedulePrepareError> prepare(bool check_error = true) {
+        // complete edges.
+        for (auto& [label, pnode] : nodes) {
+            auto& node = *pnode;
+            // validate edges
+            for (const auto& dep_label : node.edges.depends) {
+                if (auto it = nodes.find(dep_label); it != nodes.end()) {
+                    node.validated_edges.depends.insert(dep_label);
+                    it->second->validated_edges.successors.insert(label);
+                }
+            }
+            for (const auto& succ_label : node.edges.successors) {
+                if (auto it = nodes.find(succ_label); it != nodes.end()) {
+                    node.validated_edges.successors.insert(succ_label);
+                    it->second->validated_edges.depends.insert(label);
+                }
+            }
+            for (const auto& parent_label : node.edges.parents) {
+                if (auto it = nodes.find(parent_label); it != nodes.end()) {
+                    node.validated_edges.parents.insert(parent_label);
+                    it->second->validated_edges.children.insert(label);
+                }
+            }
+            for (const auto& child_label : node.edges.children) {
+                if (auto it = nodes.find(child_label); it != nodes.end()) {
+                    node.validated_edges.children.insert(child_label);
+                    it->second->validated_edges.parents.insert(label);
+                }
+            }
+        }
+        // rebuild cache
+        cache.emplace();
+        ScheduleCache& schedule_cache = *cache;
+        schedule_cache.nodes.reserve(nodes.size());
+        for (auto& [label, node] : nodes) {
+            schedule_cache.node_map.emplace(label, schedule_cache.nodes.size());
+            CachedNode& cached_node = schedule_cache.nodes.emplace_back();
+            cached_node.node        = node;
+        }
+        for (auto&& [index, cached_node] : schedule_cache.nodes | std::views::enumerate) {
+            cached_node.depends =
+                cached_node.node->validated_edges.depends |
+                std::views::transform([&](const SystemSetLabel& label) { return schedule_cache.node_map.at(label); }) |
+                std::ranges::to<std::vector<size_t>>();
+            cached_node.successors =
+                cached_node.node->validated_edges.successors |
+                std::views::transform([&](const SystemSetLabel& label) { return schedule_cache.node_map.at(label); }) |
+                std::ranges::to<std::vector<size_t>>();
+            cached_node.parents =
+                cached_node.node->validated_edges.parents |
+                std::views::transform([&](const SystemSetLabel& label) { return schedule_cache.node_map.at(label); }) |
+                std::ranges::to<std::vector<size_t>>();
+            cached_node.children =
+                cached_node.node->validated_edges.children |
+                std::views::transform([&](const SystemSetLabel& label) { return schedule_cache.node_map.at(label); }) |
+                std::ranges::to<std::vector<size_t>>();
+            if (cached_node.depends.empty() && cached_node.parents.empty()) {
+                schedule_cache.start_nodes.push_back(index);
+            }
+        }
+        // check if cycles in dependencies
+        {
+            // time complexity: O(V + E)
+            std::vector<size_t> temp_marks(schedule_cache.nodes.size(), 0);
+            std::vector<size_t> perm_marks(schedule_cache.nodes.size(), 0);
+            std::vector<SystemSetLabel> path;
+            std::function<bool(size_t)> visit = [&](size_t index) -> bool {
+                if (perm_marks[index]) return false;
+                if (temp_marks[index]) {
+                    path.push_back(schedule_cache.nodes[index].node->label);
+                    return true;
+                }
+                temp_marks[index] = 1;
+                path.push_back(schedule_cache.nodes[index].node->label);
+                for (const auto& succ_index : schedule_cache.nodes[index].successors) {
+                    if (visit(succ_index)) return true;
+                }
+                for (const auto& child_index : schedule_cache.nodes[index].children) {
+                    if (visit(child_index)) return true;
+                }
+                temp_marks[index] = 0;
+                perm_marks[index] = 1;
+                path.pop_back();
+                return false;
+            };
+            for (size_t i = 0; i < schedule_cache.nodes.size(); i++) {
+                if (!perm_marks[i]) {
+                    if (visit(i)) {
+                        return std::unexpected(SchedulePrepareError{
+                            .associated_labels = path,
+                            .type              = SchedulePrepareError::Type::CyclicDependency,
+                        });
+                    }
+                }
+            }
+        }
+        // check if cycles in hierarchy
+        {
+            // time complexity: O(V + E)
+            std::vector<size_t> temp_marks(schedule_cache.nodes.size(), 0);
+            std::vector<size_t> perm_marks(schedule_cache.nodes.size(), 0);
+            std::vector<SystemSetLabel> path;
+            std::function<bool(size_t)> visit = [&](size_t index) -> bool {
+                if (perm_marks[index]) return false;
+                if (temp_marks[index]) {
+                    path.push_back(schedule_cache.nodes[index].node->label);
+                    return true;
+                }
+                temp_marks[index] = 1;
+                path.push_back(schedule_cache.nodes[index].node->label);
+                for (const auto& child_index : schedule_cache.nodes[index].children) {
+                    if (visit(child_index)) return true;
+                }
+                temp_marks[index] = 0;
+                perm_marks[index] = 1;
+                path.pop_back();
+                return false;
+            };
+            for (size_t i = 0; i < schedule_cache.nodes.size(); i++) {
+                if (!perm_marks[i]) {
+                    if (visit(i)) {
+                        return std::unexpected(SchedulePrepareError{
+                            .associated_labels = path,
+                            .type              = SchedulePrepareError::Type::CyclicHierarchy,
+                        });
+                    }
+                }
+            }
+        }
+        // check if any nodes have parents with dependencies
+        {
+            std::vector<storage::bit_vector> reachable_parents(schedule_cache.nodes.size(),
+                                                               storage::bit_vector(schedule_cache.nodes.size()));
+            std::vector<storage::bit_vector> reachable_dependencies(schedule_cache.nodes.size(),
+                                                                    storage::bit_vector(schedule_cache.nodes.size()));
+            storage::bit_vector visited(schedule_cache.nodes.size());
+            std::function<void(size_t)> dfs_parents = [&](size_t index) {
+                if (visited.contains(index)) return;
+                visited.set(index);
+                reachable_parents[index].set(index);
+                for (const auto& parent_index : schedule_cache.nodes[index].parents) {
+                    dfs_parents(parent_index);
+                    reachable_parents[index].set(parent_index);
+                    reachable_parents[index].union_with(reachable_parents[parent_index]);
+                }
+            };
+            std::function<void(size_t)> dfs_dependencies = [&](size_t index) {
+                if (visited.contains(index)) return;
+                visited.set(index);
+                for (const auto& dep_index : schedule_cache.nodes[index].depends) {
+                    dfs_dependencies(dep_index);
+                    reachable_dependencies[index].set(dep_index);
+                    reachable_dependencies[index].union_with(reachable_dependencies[dep_index]);
+                }
+            };
+            visited.clear();
+            for (size_t i = 0; i < schedule_cache.nodes.size(); i++) {
+                dfs_parents(i);
+            }
+            visited.clear();
+            for (size_t i = 0; i < schedule_cache.nodes.size(); i++) {
+                dfs_dependencies(i);
+            }
+            // for each node, if any pair of its parents has dependencies, report error
+            for (size_t i = 0; i < schedule_cache.nodes.size(); i++) {
+                const auto& parents = schedule_cache.nodes[i].parents;
+                std::vector<SystemSetLabel> conflict_parents;
+                for (auto&& [p1, p2] : std::views::cartesian_product(parents, parents)) {
+                    if (p1 >= p2) continue;
+                    if (reachable_dependencies[schedule_cache.node_map.at(p1)].contains(
+                            schedule_cache.node_map.at(p2))) {
+                        conflict_parents.push_back(p1);
+                        conflict_parents.push_back(p2);
+                        break;
+                    }
+                }
+                if (!conflict_parents.empty()) {
+                    std::vector<SystemSetLabel> associated_labels;
+                    associated_labels.push_back(schedule_cache.nodes[i].node->label);
+                    return std::unexpected(SchedulePrepareError{
+                        .associated_labels = associated_labels,
+                        .conflict_parents  = conflict_parents,
+                        .type              = SchedulePrepareError::Type::ParentsWithDeps,
+                    });
+                }
+            }
+        }
+        return {};
+    }
+    void execute(SystemDispatcher& dispatcher) {
+        if (!cache) {
+            auto prepare_result = prepare(true);
+            // we still execute even if there are errors, cause it won't crash, just some nodes won't run
+        }
+
+        ExecutionState exec_state{
+            .running_count       = 0,
+            .remaining_count     = cache->nodes.size(),
+            .ready_nodes         = storage::bit_vector(cache->nodes.size()),
+            .running_nodes       = storage::bit_vector(cache->nodes.size()),
+            .finished_nodes      = storage::bit_vector(cache->nodes.size()),
+            .entered_nodes       = storage::bit_vector(cache->nodes.size()),
+            .condition_met_nodes = storage::bit_vector(cache->nodes.size()),
+            .wait_count          = std::vector<size_t>(cache->nodes.size(), 0),
+            .child_count         = std::vector<size_t>(cache->nodes.size(), 0),
+        };
+        // initialize cache value.
+        for (auto&& [index, cached_node] : cache->nodes | std::views::enumerate) {
+            exec_state.wait_count[index]  = cached_node.depends.size() + cached_node.parents.size();
+            exec_state.child_count[index] = cached_node.children.size() + (cached_node.node->system ? 1 : 0);
+        }
+    }
+};
+}  // namespace epix::core::schedule
---
+++ epix_engine/core/include/epix/core/schedule/system_dispatcher.hpp
@@ -0,0 +1,268 @@
+#pragma once
+
+#include <concepts>
+#include <condition_variable>
+#include <deque>
+#include <expected>
+#include <future>
+#include <mutex>
+#include <ranges>
+#include <vector>
+
+#define BS_THREAD_POOL_NATIVE_EXTENSIONS
+#include <BS_thread_pool.hpp>
+
+#include "../query/access.hpp"
+#include "../system/system.hpp"
+
+namespace epix::core::schedule {
+struct smallvec : std::ranges::view_interface<smallvec> {
+   public:
+    smallvec() {}
+    smallvec(const smallvec& other) : size_(other.size_) {
+        if (other.is_small()) {
+            std::copy(other.small_array, other.small_array + other.size_, small_array);
+        } else {
+            new (&large_array) std::vector<size_t>(other.large_array);
+        }
+    }
+    smallvec(smallvec&& other) noexcept : size_(other.size_) {
+        if (other.is_small()) {
+            std::copy(other.small_array, other.small_array + other.size_, small_array);
+        } else {
+            new (&large_array) std::vector<size_t>(std::move(other.large_array));
+            other.large_array.~vector();
+            other.size_ = 0;
+        }
+    }
+    ~smallvec() {
+        if (!is_small()) {
+            large_array.~vector();
+        }
+    }
+
+    void push_back(size_t value) {
+        if (is_small()) {
+            if (size_ < 4) {
+                small_array[size_++] = value;
+            } else {
+                // move to large vector
+                new (&large_array) std::vector<size_t>(small_array, small_array + size_);
+                large_array.push_back(value);
+                size_++;
+            }
+        } else {
+            large_array.push_back(value);
+            size_++;
+        }
+    }
+    size_t pop_back() {
+        if (is_small()) {
+            return small_array[--size_];
+        } else {
+            size_--;
+            size_t value = large_array.back();
+            large_array.pop_back();
+            if (size_ <= 4) {
+                size_t cache[4];
+                for (size_t i = 0; i < size_; i++) {
+                    cache[i] = large_array[i];
+                }
+                large_array.~vector();
+                std::copy(cache, cache + size_, small_array);
+            }
+            return value;
+        }
+    }
+
+    bool empty() const { return size_ == 0; }
+    size_t size() const { return size_; }
+    size_t* begin() {
+        if (is_small()) {
+            return &small_array[0];
+        } else {
+            return large_array.data();
+        }
+    }
+    size_t* end() {
+        if (is_small()) {
+            return &small_array[size_];
+        } else {
+            return large_array.data() + large_array.size();
+        }
+    }
+
+   private:
+    union {
+        size_t small_array[4];
+        std::vector<size_t> large_array;
+    };
+    size_t size_ = 0;
+
+    bool is_small() const { return size_ < 4; }
+};
+struct async_queue {
+    mutable std::mutex mutex;
+    std::condition_variable condition;
+    smallvec queue;
+
+    void push(size_t index) {
+        std::unique_lock<std::mutex> lock(mutex);
+        queue.push_back(index);
+        condition.notify_one();
+    }
+
+    smallvec pop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        condition.wait(lock, [this] { return !queue.empty(); });
+        return std::move(queue);
+    }
+    smallvec try_pop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        return std::move(queue);
+    }
+
+    size_t size() const {
+        std::unique_lock<std::mutex> lock(mutex);
+        return queue.size();
+    }
+
+    bool empty() const {
+        std::unique_lock<std::mutex> lock(mutex);
+        return queue.empty();
+    }
+};
+struct DispatchConfig {
+    std::function<void()> on_finish = nullptr;
+};
+struct SystemDispatcher {
+   private:
+    std::vector<const query::FilteredAccessSet*> system_accesses;
+    std::deque<size_t> free_indices;
+    std::deque<std::tuple<const query::FilteredAccessSet*, DispatchConfig, std::function<void(size_t)>>>
+        pending_systems;
+    async_queue finished_queue;
+    World* world = nullptr;
+    std::mutex mutex_;
+    BS::thread_pool<BS::tp::none> thread_pool;
+    system::SystemUnique<system::In<std::function<void(World&)>>> world_scope_system;
+    query::FilteredAccessSet world_scope_access;
+
+   public:
+    SystemDispatcher(World& world_ref, size_t thread_count = std::clamp(std::thread::hardware_concurrency(), 2u, 8u))
+        : world(&world_ref),
+          thread_pool(thread_count),
+          world_scope_system(system::make_system(
+              [](system::In<std::function<void(World&)>> input, World& world) { input.get()(world); })) {
+        // initializing this system accesses mut world but does not read or write to it
+        world_scope_access = world_scope_system->initialize(world_ref);
+    }
+    ~SystemDispatcher() {
+        // we do not need to explicitly join the thread pool, as it will be joined in its destructor, but we
+        // shell be prepared for future changes which might use external thread pools that is not destructed here.
+
+        // since the submit order is preserved, and a World& access is not compatible with any other access.
+        // just dispatch a world scope system to wait for all systems to finish.
+        world_scope([](World& world) {}).wait();
+    }
+    bool tick() {
+        // collect finished systems
+        std::unique_lock<std::mutex> lock(mutex_);
+        auto finished = finished_queue.try_pop();
+        if (finished.empty()) {
+            return false;
+        }
+        for (auto&& index : finished) {
+            const auto& access     = system_accesses[index];
+            system_accesses[index] = nullptr;
+            free_indices.push_back(index);
+        }
+        // check pending systems
+        auto get_index = [&]() -> size_t {
+            if (!free_indices.empty()) {
+                size_t index = free_indices.front();
+                free_indices.pop_front();
+                return index;
+            } else {
+                size_t index = system_accesses.size();
+                system_accesses.push_back(nullptr);
+                return index;
+            }
+        };
+        while (!pending_systems.empty()) {
+            const auto& [access, config, func] = pending_systems.front();
+            // check for conflicts
+            bool conflict = false;
+            for (const auto& existing_access : system_accesses) {
+                if (existing_access && !access->is_compatible(*existing_access)) {
+                    conflict = true;
+                    break;
+                }
+            }
+            if (conflict) break;
+            // can schedule
+            size_t index           = get_index();
+            system_accesses[index] = access;
+            thread_pool.detach_task([func = std::move(func), index] { return func(index); });
+            pending_systems.pop_front();
+        }
+        return true;
+    }
+
+    std::future<std::expected<void, system::RunSystemError>> world_scope(std::invocable<World&> auto&& func) {
+        return dispatch_system(*world_scope_system, std::function(func), world_scope_access);
+    }
+    // Caller has to guarantee that the system and access pointers are valid during the execution
+    template <typename In, typename Out>
+    std::future<std::expected<Out, system::RunSystemError>> dispatch_system(system::System<In, Out>& sys,
+                                                                            system::SystemInput<In>::Input input,
+                                                                            const query::FilteredAccessSet& access,
+                                                                            DispatchConfig config = {}) {
+        std::packaged_task<std::expected<Out, system::RunSystemError>()> task(
+            [this, &sys, input = std::move(input), access]() mutable {
+                return sys.run_no_apply(std::move(input), *world);
+            });
+        auto fut = task.get_future();
+        std::unique_lock<std::mutex> lock(mutex_);
+        pending_systems.emplace_back(&access, config,
+                                     [task = std::move(task), on_finished = std::move(config.on_finish),
+                                      fut = std::move(fut), this](size_t index) mutable {
+                                         task();
+                                         if (on_finished) on_finished();
+                                         finished_queue.push(index);
+                                         tick();  // tick whenever a system finishes
+                                     });
+        return fut;
+    }
+    template <typename In, typename Out>
+    std::optional<std::expected<Out, system::RunSystemError>> try_run_system(system::System<In, Out>& sys,
+                                                                             system::SystemInput<In>::Input input,
+                                                                             const query::FilteredAccessSet& access) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        // check for conflicts
+        for (const auto& existing_access : system_accesses) {
+            if (existing_access && !access.is_compatible(*existing_access)) {
+                return std::nullopt;
+            }
+        }
+        // run on current thread
+        return sys.run_no_apply(std::move(input), *world);
+    }
+    template <typename In, typename Out>
+    std::future<void> apply_deferred(system::System<In, Out>& sys) {
+        return world_scope([&](World& world) { sys.apply_deferred(world); });
+    }
+    template <typename T>
+    std::future<void> apply_deferred(T&& range)
+        requires std::ranges::viewable_range<T> && requires(std::ranges::range_reference_t<T> ref, World& world) {
+            { ref.apply_deferred(world) } -> std::same_as<void>;
+        }
+    {
+        return world_scope([&](World& world) {
+            for (auto&& sys : range) {
+                sys.apply_deferred(world);
+            }
+        });
+    }
+};
+}  // namespace epix::core::schedule
