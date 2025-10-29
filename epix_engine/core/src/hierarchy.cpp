cb03035f3bcd9e7d3aadc93c2f73079fc7d48253
Etern Maxwell <maxwell_dev@outlook.com>
2025-10-29 19:52:44 +0800

    corrected children hooks

--- epix_engine/core/src/hierarchy.cpp
+++ epix_engine/core/src/hierarchy.cpp
@@ -37,8 +37,11 @@ void Parent::on_insert(World& world, HookContext ctx) {
 
 void Children::on_remove(World& world, HookContext ctx) {
     auto&& this_entity = world.entity_mut(ctx.entity);
-    this_entity.get<Children>().transform([&](const Children& children) {
-        for (auto child_entity : children.entities) {
+    // copy children set cause removing it inside transform or and_then might invalidate the reference
+    std::optional children_to_remove =
+        this_entity.get<Children>().transform([&](const Children& children) { return children.entities; });
+    children_to_remove.transform([&](std::unordered_set<Entity>& children) {
+        for (auto child_entity : children) {
             world.entity_mut(child_entity).remove<Parent>();
         }
         return true;
@@ -47,8 +50,10 @@ void Children::on_remove(World& world, HookContext ctx) {
 
 void Children::on_despawn(World& world, HookContext ctx) {
     auto&& this_entity = world.entity_mut(ctx.entity);
-    this_entity.get<Children>().transform([&](const Children& children) {
-        for (auto child_entity : children.entities) {
+    std::optional children_to_remove =
+        this_entity.get<Children>().transform([&](const Children& children) { return children.entities; });
+    children_to_remove.transform([&](std::unordered_set<Entity>& children) {
+        for (auto child_entity : children) {
             world.entity_mut(child_entity).despawn();
         }
         return true;
