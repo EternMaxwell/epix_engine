5f76a345b53c05f5f421a0abb6aab8399b136a12
Etern Maxwell <maxwell_dev@outlook.com>
2025-11-29 00:40:07 +0800

    optimized calculate global transform in TransformPlugin by only recalculating entities which Transform is changed.

--- epix_engine/transform/src/plugin.cpp
+++ epix_engine/transform/src/plugin.cpp
@@ -1,3 +1,7 @@
+#include <ranges>
+#include <stack>
+
+#include "epix/core.hpp"
 #include "epix/transform.hpp"
 
 using namespace epix;
@@ -9,31 +13,71 @@ void insert_global_transform(Commands cmd,
         cmd.entity(entity).insert(GlobalTransform{.matrix = glm::mat4(1.0f)});
     }
 }
-void calculate_global_transform(Query<Item<Entity, const Transform&, Opt<const Parent&>, Mut<GlobalTransform>>> query) {
-    std::deque<std::tuple<const Transform&, const Parent&, GlobalTransform&>> toProcess;
-    std::unordered_map<Entity, GlobalTransform*> globalTransforms;
-    for (auto&& [entity, transform, parent, globalTransform] : query.iter()) {
-        if (parent) {
-            toProcess.emplace_back(transform, *parent, globalTransform);
-        } else {
-            globalTransform->matrix  = transform.to_matrix();
-            globalTransforms[entity] = globalTransform.ptr_mut();
+void calculate_global_transform(
+    Query<Item<Entity, Ref<Transform>, Opt<const Children&>, Opt<const Parent&>, Mut<GlobalTransform>>> query) {
+    std::unordered_map<Entity, Entity> change_root;
+    for (auto&& [entity, transform, children, parent, globalTransform] : query.iter()) {
+        if (transform.is_modified() || transform.is_added()) {
+            Entity root;
+            if (auto root_it = change_root.find(entity); root_it != change_root.end()) {
+                root = root_it->second;
+            } else {
+                root                = entity;
+                change_root[entity] = entity;
+            }
+            std::stack<Entity> child_stack;
+            child_stack.push(entity);
+            while (!child_stack.empty()) {
+                Entity current = child_stack.top();
+                child_stack.pop();
+                if (children.has_value()) {
+                    for (const auto& child : children->get().entities) {
+                        child_stack.push(child);
+                        change_root[child] = root;
+                    }
+                }
+            }
         }
     }
-    while (!toProcess.empty()) {
-        auto [transform, parent, globalTransform] = toProcess.front();
-        toProcess.pop_front();
+    for (auto entity : change_root | std::views::filter([&](const auto& pair) { return pair.first == pair.second; }) |
+                           std::views::keys) {
+        auto [_, transform, children, parent, globalTransform] = query.get(entity).value();
 
-        auto it = globalTransforms.find(parent.entity);
-        if (it != globalTransforms.end()) {
-            globalTransform.matrix = it->second->matrix * transform.to_matrix();
-        } else {
-            toProcess.emplace_back(transform, parent, globalTransform);
+        globalTransform.get_mut() = GlobalTransform{
+            .matrix = transform.get().to_matrix(),
+        };
+        if (children.has_value()) {
+            std::stack<Entity> child_stack;
+            for (const auto& child : children->get().entities) {
+                auto [_, child_transform, child_children, child_parent, child_globalTransform] =
+                    query.get(child).value();
+                child_globalTransform.get_mut() = GlobalTransform{
+                    .matrix = globalTransform.get_mut().matrix * child_transform.get().to_matrix(),
+                };
+                if (child_children.has_value()) child_stack.push(child);
+            }
+            while (!child_stack.empty()) {
+                Entity current = child_stack.top();
+                child_stack.pop();
+                auto [_, current_transform, current_children, current_parent, current_globalTransform] =
+                    query.get(current).value();
+                if (current_children.has_value()) {
+                    for (const auto& child : current_children->get().entities) {
+                        auto [_, child_transform, child_children, child_parent, child_globalTransform] =
+                            query.get(child).value();
+                        child_globalTransform.get_mut() = GlobalTransform{
+                            .matrix = current_globalTransform.get_mut().matrix * child_transform.get().to_matrix(),
+                        };
+                        if (child_children.has_value()) child_stack.push(child);
+                    }
+                }
+            }
         }
     }
 }
 
 void TransformPlugin::build(epix::App& app) {
+    app.configure_sets(sets(TransformSets::CalculateGlobalTransform));
     app.add_systems(
         Last,
         into(into(calculate_global_transform)
