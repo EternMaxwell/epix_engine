38871d83cf8f5d11d2f324c1fa4f9c37db8bbe14
Etern Maxwell <maxwell_dev@outlook.com>
2026-02-26 01:09:33 +0800

    glfw render module, for supporting window render for windows with glfw backend.

---
+++ epix_engine/render/modules/glfw_render/modules/glfw_render.cppm
@@ -0,0 +1,9 @@
+﻿export module epix.glfw.render;
+
+import epix.core;
+
+namespace glfw::render {
+export struct GlfwRenderPlugin {
+    void build(core::App& app);
+};
+}  // namespace glfw::render
---
+++ epix_engine/render/modules/glfw_render/src/glfw_render.cpp
@@ -0,0 +1,179 @@
+﻿module;
+
+#define WGPU_TARGET_MACOS 1
+#define WGPU_TARGET_LINUX_X11 2
+#define WGPU_TARGET_WINDOWS 3
+#define WGPU_TARGET_LINUX_WAYLAND 4
+#define WGPU_TARGET_EMSCRIPTEN 5
+
+#if defined(__EMSCRIPTEN__)
+#define WGPU_TARGET WGPU_TARGET_EMSCRIPTEN
+#elif defined(_WIN32)
+#define WGPU_TARGET WGPU_TARGET_WINDOWS
+#elif defined(__APPLE__)
+#define WGPU_TARGET WGPU_TARGET_MACOS
+#elif defined(_GLFW_WAYLAND)
+#define WGPU_TARGET WGPU_TARGET_LINUX_WAYLAND
+#else
+#define WGPU_TARGET WGPU_TARGET_LINUX_X11
+#endif
+
+#if WGPU_TARGET == WGPU_TARGET_MACOS
+#include <Foundation/Foundation.h>
+#include <QuartzCore/CAMetalLayer.h>
+#endif
+
+#include <GLFW/glfw3.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif  // __EMSCRIPTEN__
+
+#if WGPU_TARGET == WGPU_TARGET_MACOS
+#define GLFW_EXPOSE_NATIVE_COCOA
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_X11
+#define GLFW_EXPOSE_NATIVE_X11
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_WAYLAND
+#define GLFW_EXPOSE_NATIVE_WAYLAND
+#elif WGPU_TARGET == WGPU_TARGET_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+
+#if !defined(__EMSCRIPTEN__)
+#include <GLFW/glfw3native.h>
+#endif
+#include <webgpu/webgpu.h>
+
+module epix.glfw.render;
+
+import std;
+import epix.core;
+import epix.render;
+import epix.glfw.core;
+import epix.window;
+import webgpu;
+
+WGPUSurface glfwGetWGPUSurfaceRaw(WGPUInstance instance, GLFWwindow* window) {
+#if WGPU_TARGET == WGPU_TARGET_MACOS
+    {
+        id metal_layer      = [CAMetalLayer layer];
+        NSWindow* ns_window = glfwGetCocoaWindow(window);
+        [ns_window.contentView setWantsLayer:YES];
+        [ns_window.contentView setLayer:metal_layer];
+
+        WGPUSurfaceSourceMetalLayer fromMetalLayer;
+        fromMetalLayer.chain.next  = NULL;
+        fromMetalLayer.chain.sType = WGPUSType_SurfaceSourceMetalLayer;
+        fromMetalLayer.layer       = metal_layer;
+
+        WGPUSurfaceDescriptor surfaceDescriptor;
+        surfaceDescriptor.nextInChain = &fromMetalLayer.chain;
+        surfaceDescriptor.label       = {NULL, WGPU_STRLEN};
+
+        return wgpuInstanceCreateSurface(instance, &surfaceDescriptor);
+    }
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_X11
+    {
+        Display* x11_display = glfwGetX11Display();
+        Window x11_window    = glfwGetX11Window(window);
+
+        WGPUSurfaceSourceXlibWindow fromXlibWindow;
+        fromXlibWindow.chain.next  = NULL;
+        fromXlibWindow.chain.sType = WGPUSType_SurfaceSourceXlibWindow;
+        fromXlibWindow.display     = x11_display;
+        fromXlibWindow.window      = x11_window;
+
+        WGPUSurfaceDescriptor surfaceDescriptor;
+        surfaceDescriptor.nextInChain = &fromXlibWindow.chain;
+        surfaceDescriptor.label       = {NULL, WGPU_STRLEN};
+
+        return wgpuInstanceCreateSurface(instance, &surfaceDescriptor);
+    }
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_WAYLAND
+    {
+        struct wl_display* wayland_display = glfwGetWaylandDisplay();
+        struct wl_surface* wayland_surface = glfwGetWaylandWindow(window);
+
+        WGPUSurfaceSourceWaylandSurface fromWaylandSurface;
+        fromWaylandSurface.chain.next  = NULL;
+        fromWaylandSurface.chain.sType = WGPUSType_SurfaceSourceWaylandSurface;
+        fromWaylandSurface.display     = wayland_display;
+        fromWaylandSurface.surface     = wayland_surface;
+
+        WGPUSurfaceDescriptor surfaceDescriptor;
+        surfaceDescriptor.nextInChain = &fromWaylandSurface.chain;
+        surfaceDescriptor.label       = {NULL, WGPU_STRLEN};
+
+        return wgpuInstanceCreateSurface(instance, &surfaceDescriptor);
+    }
+#elif WGPU_TARGET == WGPU_TARGET_WINDOWS
+    {
+        HWND hwnd           = glfwGetWin32Window(window);
+        HINSTANCE hinstance = GetModuleHandle(NULL);
+
+        WGPUSurfaceSourceWindowsHWND fromWindowsHWND;
+        fromWindowsHWND.chain.next  = NULL;
+        fromWindowsHWND.chain.sType = WGPUSType_SurfaceSourceWindowsHWND;
+        fromWindowsHWND.hinstance   = hinstance;
+        fromWindowsHWND.hwnd        = hwnd;
+
+        WGPUSurfaceDescriptor surfaceDescriptor;
+        surfaceDescriptor.nextInChain = &fromWindowsHWND.chain;
+        surfaceDescriptor.label       = {NULL, WGPU_STRLEN};
+
+        return wgpuInstanceCreateSurface(instance, &surfaceDescriptor);
+    }
+#elif WGPU_TARGET == WGPU_TARGET_EMSCRIPTEN
+    {
+        WGPUSurfaceSourceCanvasHTMLSelector fromCanvasHTMLSelector;
+        fromCanvasHTMLSelector.chain.next  = NULL;
+        fromCanvasHTMLSelector.chain.sType = WGPUSType_SurfaceSourceCanvasHTMLSelector;
+        fromCanvasHTMLSelector.selector    = "canvas";
+
+        WGPUSurfaceDescriptor surfaceDescriptor;
+        surfaceDescriptor.nextInChain = &fromCanvasHTMLSelector.chain;
+        surfaceDescriptor.label       = {NULL, WGPU_STRLEN};
+
+        return wgpuInstanceCreateSurface(instance, &surfaceDescriptor);
+    }
+#else
+#error "Unsupported WGPU_TARGET"
+#endif
+}
+
+wgpu::Surface glfwGetWGPUSurface(const wgpu::Instance& instance, GLFWwindow* window) {
+    auto res = glfwGetWGPUSurfaceRaw(instance, window);
+    return std::move(*reinterpret_cast<wgpu::Surface*>(&res));
+}
+
+using namespace core;
+
+using render::window::SurfaceCreation;
+
+void glfw::render::GlfwRenderPlugin::build(App& app) {
+    auto system = make_system_unique(
+        [](Commands commands, Query<Item<Entity>, Filter<With<window::Window>, Without<SurfaceCreation>>> windows,
+           ResMut<GLFWwindows> glfw_windows) {
+            for (auto&& [id] : windows.iter()) {
+                auto it = glfw_windows->find(id);
+                if (it == glfw_windows->end()) continue;
+                GLFWwindow* glfw_window = it->second.first;
+                if (!glfw_window) continue;
+                commands.entity(id).insert(SurfaceCreation([glfw_window](const wgpu::Instance& instance) {
+                    return glfwGetWGPUSurface(instance, glfw_window);
+                }));
+            }
+        });
+    app.runner_scope([system = std::move(system)](GLFWRunner& runner) mutable {
+           runner.set_render_app(::render::Render);
+           runner.append_system(std::move(system));
+       })
+        .transform_error([](App::RunnerError error) {
+            if (error == App::RunnerError::RunnerNotSet) {
+                throw std::runtime_error("GlfwRenderPlugin requires an AppRunner to be set before building");
+            } else if (error == App::RunnerError::RunnerMismatch) {
+                throw std::runtime_error("GlfwRenderPlugin requires a GLFWRunner as the AppRunner");
+            }
+            return error;
+        });
+}
