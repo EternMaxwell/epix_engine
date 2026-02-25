467735f1425e19e14c6bec4d23bde8b4e8f8e77e
Etern Maxwell <maxwell_dev@outlook.com>
2026-02-26 01:07:27 +0800

    image render.

---
+++ epix_engine/render/modules/render/modules/image.cppm
@@ -0,0 +1,26 @@
+﻿module;
+
+export module epix.render:image;
+
+import :assets;
+
+import epix.image;
+import epix.assets;
+import webgpu;
+
+namespace render {
+export struct GPUImage {
+    wgpu::Texture texture;
+    wgpu::TextureView view;
+    wgpu::Sampler sampler;
+};
+}  // namespace render
+
+template <>
+struct render::RenderAsset<image::Image> {
+    using Param          = std::tuple<Res<wgpu::Device>, Res<wgpu::Queue>>;
+    using ProcessedAsset = GPUImage;
+
+    ProcessedAsset process(image::Image&& asset, Param param);
+    RenderAssetUsage usage(const image::Image& asset);
+};
---
+++ epix_engine/render/modules/render/src/image.cpp
@@ -0,0 +1,73 @@
+﻿module;
+
+module epix.render;
+
+import :image;
+
+using namespace render;
+
+wgpu::TextureFormat format_cast(image::Format format) {
+    switch (format) {
+        case image::Format::Grey8:
+            return wgpu::TextureFormat::eR8Unorm;
+        case image::Format::GreyAlpha8:
+            return wgpu::TextureFormat::eRG8Unorm;
+        case image::Format::RGB8:
+            return wgpu::TextureFormat::eUndefined;  // no direct mapping, need to convert to RGBA8
+        case image::Format::RGBA8:
+            return wgpu::TextureFormat::eRGBA8Unorm;
+        case image::Format::Grey16:
+            return wgpu::TextureFormat::eR16Uint;
+        case image::Format::RGB16:
+            return wgpu::TextureFormat::eUndefined;  // no direct mapping, need to convert to RGBA16
+        case image::Format::RGBA16:
+            return wgpu::TextureFormat::eRGBA16Uint;
+        case image::Format::Grey32F:
+            return wgpu::TextureFormat::eR32Float;
+        case image::Format::RGB32F:
+            return wgpu::TextureFormat::eUndefined;  // no direct mapping, need to convert to RGBA32F
+        case image::Format::RGBA32F:
+            return wgpu::TextureFormat::eRGBA32Float;
+        default:
+            return wgpu::TextureFormat::eUndefined;
+    }
+}
+
+GPUImage render::RenderAsset<image::Image>::process(image::Image&& asset, Param param) {
+    auto& [device, queue] = param;
+    wgpu::TextureDescriptor desc;
+    desc.setUsage(wgpu::TextureUsage::eCopyDst | wgpu::TextureUsage::eTextureBinding)
+        .setDimension(wgpu::TextureDimension::e2D)
+        .setFormat(format_cast(asset.format()))
+        .setSize({asset.width(), asset.height(), 1})
+        .setMipLevelCount(1)
+        .setSampleCount(1);
+
+    if (desc.format == wgpu::TextureFormat::eUndefined) {
+        throw std::runtime_error("Unsupported image format for GPU upload");
+    }
+
+    GPUImage gpu_image;
+    gpu_image.texture = device->createTexture(desc);
+    gpu_image.view    = gpu_image.texture.createView();
+    gpu_image.sampler = device->createSampler(
+        wgpu::SamplerDescriptor().setMinFilter(wgpu::FilterMode::eLinear).setMagFilter(wgpu::FilterMode::eLinear));
+
+    auto view = asset.raw_view();
+
+    queue->writeTexture(wgpu::TexelCopyTextureInfo()
+                            .setTexture(gpu_image.texture)
+                            .setOrigin({0, 0, 0})
+                            .setAspect(wgpu::TextureAspect::eAll),
+                        view.data(), view.size_bytes(),
+                        wgpu::TexelCopyBufferLayout()
+                            .setBytesPerRow(asset.width() * asset.format_info().pixelSize())
+                            .setRowsPerImage(asset.height()),
+                        wgpu::Extent3D{asset.width(), asset.height(), 1});
+
+    return gpu_image;
+}
+
+render::RenderAssetUsage render::RenderAsset<image::Image>::usage(const image::Image& asset) {
+    return static_cast<RenderAssetUsage>(asset.usage());
+}
